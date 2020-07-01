9ded0c50e1f312ce3ed825f73b65bed769b64412
efehoplamaz <hopefe.17@gmail.com>
2020-07-01 12:45:20 +0100

    Midway commit

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+$(document).ready(function(){
+  $("button.scroll-to-aboutme").click(function(){
+    $('body,html').animate({ scrollTop: $('.about_me').offset().top}, 1200);
+  });
+  $("button.scroll-to-projects").click(function(){
+    $('body,html').animate({ scrollTop: $('.project_section').offset().top}, 1200);
+  });
+  $("button.scroll-to-aboutme").hover(function(){
+    $(this).css("background-color", "yellow");
+    }, function(){
+    $(this).css("background-color", "pink");
+  });
+});
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
 
-function showSlides(n) {
+
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
   var i;
-  var slides = document.getElementsByClassName("main");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2200); // Change image every 2 seconds
 }
