c5b447536f50fe79d0762f94bca974185adec91f
efehoplamaz <hopefe.17@gmail.com>
2021-01-31 01:39:35 +0300

    Removed about me section, will add later

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 $(document).ready(function(){
+
+  //scroll down animations
   $("button.scroll-to-aboutme").click(function(){
+    $('body,html').animate({ scrollTop: $('.resume').offset().top}, 1200);
+  });
+  $("button.scroll-to-resume").click(function(){
+    $('body,html').animate({ scrollTop: $('.resume').offset().top}, 1200);
+  });
+  $("div.about_me_header").click(function(){
     $('body,html').animate({ scrollTop: $('.about_me').offset().top}, 1200);
   });
-  $("button.scroll-to-projects").click(function(){
-    $('body,html').animate({ scrollTop: $('.project_section').offset().top}, 1200);
+
+  //Links to URLs
+  $('div.github_project_box.camera').click(function() {
+    window.open("https://github.com/efehoplamaz/CameraClara", '_blank');
   });
+  $('div.github_project_box.NLP').click(function() {
+    window.open("https://github.com/efehoplamaz/Turkish-NLP-text-classification-with-k-Nearest-Neighbors-algorithm", '_blank');
+  });
+  $('div.github_project_box.kmeans').click(function() {
+    window.open("https://github.com/efehoplamaz/Hierarchical_K-means", '_blank');
+  });
+
+  // Hover
+  $('div.github_project_box').bind('mouseenter', function() {
+    $(this).css('border', '5px solid yellow');
+  });
+  $('div.github_project_box').bind('mouseleave', function() {
+    $(this).css('border', '1px solid #e1e4e8');
+  });
+
 });
 
 
